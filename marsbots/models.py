026c7eb4b7dc6163c3051edc866950c6e03198f5
Jonathan Miller <jon@jmill.dev>
2023-01-19 21:44:25 -0800

    change

--- marsbots/models.py
+++ marsbots/models.py
@@ -3,7 +3,36 @@ from typing import Any, Callable
 
 
 @dataclass
+class BehaviorDocument:
+    name: str
+    type: str
+    data: dict
+
+
+@dataclass
+class CheckDocument:
+    type: str
+    data: dict
+
+
+@dataclass
 class Capability:
     trigger: Callable
     checks: list[Callable]
     behaviors: list[Any]
+
+
+@dataclass
+class CapabilityDocument:
+    name: str
+    trigger: str
+    behaviors: list[BehaviorDocument]
+    checks: list[CheckDocument]
+    requiredPowers: list[str]
+
+
+@dataclass
+class Personality:
+    name: str
+    behaviors: list[BehaviorDocument]
+    capabilities: list[Capability]
